66f32cbe7e27732a490915a8fd7945025cd4dbfc
mdrohan <mdsajed210@gmail.com>
2023-03-28 00:29:15 +0600

    added cart component and order summary

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -8,8 +8,8 @@ const Header = () => {
       <img src={logo} alt="" />
       <div>
         <a href="/shop">Shop</a>
-        <a href="/order">Orders</a>
-        <a href="/inventory">Inventory</a>
+        <a href="/order">Order Review</a>
+        <a href="/inventory">Manage Inventory</a>
         <a href="/login">Login</a>
       </div>
     </nav>
